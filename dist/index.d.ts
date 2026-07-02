2c23b0e1d96c6502d200cc7a209707fa903f9c0a
MAC MattX <workmac@mattx.cz>
2026-07-02 11:20:08 +0200

    cleanup

--- dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Instagram } from './nodes/Instagram.node';
-import { InstagramCredentials } from './nodes/InstagramCredentials';
-export { Instagram, InstagramCredentials };
-export { InstagramClient } from './lib/client';
-export * from './lib/types';
-export { Utils } from './lib/utils';
